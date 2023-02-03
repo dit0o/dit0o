bd3efbfec7a06900a668d545eb826cef85fd58b8
dit0o <gaganam6985@gmail.com>
2023-02-03 12:55:00 +0530

    complete expense traker

--- gagan/async.js
+++ gagan/async.js
@@ -1,84 +1,125 @@
-window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/250718b1a32d4b28ab8dd2f71fe8be58/gun")
-      .then((response) => {
-        console.log(response)
-        for (var i = 0; i < response.data.length; i++) {
-          showUsersOnScreen(response.data[i])
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+window.addEventListener("DOMContentLoaded", async() => {try{
+  
+  await axios.get("https://crudcrud.com/api/8d1572b7d4ce4589aa81bbf46ffb62cb/gun")
+    .then((response) => {
+      console.log(response)
+      for (var i = 0; i < response.data.length; i++) {
+        showUsersOnScreen(response.data[i])
+      }
+    })
+
+  
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
+  catch(error) {
+    console.log(error);
+  }
+  
+})
+
+
+async function formsubmit(event) {
+  try {
+    event.preventDefault();
+    var user = await {
+      Expenseamount: document.getElementById('Expense').value,
+      discription: document.getElementById('discription').value,
+      categeory: document.getElementById('list').value
+    }
+  
+  
+    await axios.post("https://crudcrud.com/api/8d1572b7d4ce4589aa81bbf46ffb62cb/gun", user)
+    .then((response) => {
+      console.log(response)
+     
+      showUsersOnScreen(user) 
     })
     
-     function formsubmit(event){
-  
+  } catch (error) {
+    console.log(error);
+  }
+ 
+
+
+}
+
+
+
+
+async function DeleteUser(userId) {
+  try {
+    await axios.delete(`https://crudcrud.com/api/8d1572b7d4ce4589aa81bbf46ffb62cb/gun/${userId}`)
+    .then((response) => {
+      console.log(response)
+     
+      removeFromScreen(userId)
+      
+    })
     
-      event.preventDefault();
-      var user = {
-        Expenseamount: document.getElementById('Expense').value,
-        discription: document.getElementById('discription').value,
-        categeory: document.getElementById('list').value
-      }
-  
-      axios.post("https://crudcrud.com/api/250718b1a32d4b28ab8dd2f71fe8be58/gun", user)
-        
-           showUsersOnScreen(user)
-    }
-  
-  
-  
-  function DeleteUser(userId){
-  
-  
-    axios.delete(`https://crudcrud.com/api/250718b1a32d4b28ab8dd2f71fe8be58/gun/${userId}`)
-      
-        
-        removeFromScreen(userId)
-        
-      
-      
-      
-    }
-  
-  
-  
-  function removeFromScreen(userId) {
-    var parentNode = document.getElementById('listOfUsers');
-    var childNodeToBeDeleted = document.getElementById(userId);
+    
+  } catch (error) {
+    console.log(error)
+  }
+
+
+}
+
+
+
+ async function removeFromScreen(userId) {
+  try {
+    var parentNode =  document.getElementById('listOfUsers');
+    var childNodeToBeDeleted =  document.getElementById(userId);
   
     parentNode.removeChild(childNodeToBeDeleted);
   
-  
+    
+  } catch (error) {
+    console.log(error)
   }
-  
-  
-  
-  
-  
-  function editUserDetails(Expenseamount, discription, categeory, userId) {
-  
-  
+ 
+
+}
+
+
+
+
+
+ function editUserDetails(Expenseamount, discription, categeory, userId) {
+  try {
     document.getElementById('Expense').value = Expenseamount;
-    document.getElementById('discription').value = discription;
-    document.getElementById('list').value = categeory;
-    DeleteUser(userId)
-  
+  document.getElementById('discription').value = discription;
+  document.getElementById('list').value = categeory;
+  DeleteUser(userId)
+    
+  } catch (error) {
+    console.log(error)
   }
+
+
   
-  
-  
-  function showUsersOnScreen(user) {
-  
-    var parentNode = document.getElementById('listOfUsers');
-    var childNode = `<li id=${user._id}>${user.Expenseamount}-${user.discription}-${user.categeory}
-         <button id="edit" onclick=editUserDetails('${user.Expenseamount}','${user.discription}','${user.categeory}','${user._id}')>Edit User</button>
-         <button id='${user._id}' onclick=DeleteUser('${user._id}')>Delete User</button></li>`
+
+}
+
+
+
+ async function showUsersOnScreen(user) {
+  try {
+    var parentNode =await document.getElementById('listOfUsers');
+    var childNode = await `<li id=${user._id}>${user.Expenseamount}-${user.discription}-${user.categeory}
+           <button id="edit" onclick=editUserDetails('${user.Expenseamount}','${user.discription}','${user.categeory}','${user._id}')>Edit User</button>
+           <button id='${user._id}' onclick=DeleteUser('${user._id}')>Delete User</button></li>`
     parentNode.innerHTML = parentNode.innerHTML + childNode;
-  
+    
+  } catch (error) {
+    console.log(error)
   }
+   
   
-  
-  
-  
-  
+ 
+  }
+
+
+

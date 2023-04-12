d8160086bf9817c3ef25fe5e16c80d7fcc54ae10
dit0o <gaganam6985@gmail.com>
2023-04-12 18:20:06 +0530

    new project

--- gagan/async.js
+++ gagan/async.js
@@ -1,125 +1,23 @@
-window.addEventListener("DOMContentLoaded", async() => {try{
-  
-  await axios.get("https://crudcrud.com/api/8d1572b7d4ce4589aa81bbf46ffb62cb/gun")
-    .then((response) => {
-      console.log(response)
-      for (var i = 0; i < response.data.length; i++) {
-        showUsersOnScreen(response.data[i])
-      }
-    })
-
-  
-    .catch((error)=>{
-      console.log(error);
-    })
-  }
-  catch(error) {
-    console.log(error);
-  }
-  
-})
-
-
-async function formsubmit(event) {
+async function formsubmit(event){
   try {
     event.preventDefault();
-    var user = await {
-      Expenseamount: document.getElementById('Expense').value,
-      discription: document.getElementById('discription').value,
-      categeory: document.getElementById('list').value
+    var user= await{
+      Name:document.getElementById('Name').value,
+      Email:document.getElementById('Email').value,
+      Password:document.getElementById('pass').value
     }
-  
-  
-    await axios.post("https://crudcrud.com/api/8d1572b7d4ce4589aa81bbf46ffb62cb/gun", user)
-    .then((response) => {
-      console.log(response)
-     
-      showUsersOnScreen(user) 
+    axios.post("http://localhost:3000/user/post", user)
+    .then((response)=>{
+      if(response.status===200){
+        window.location.href="../login/login.html"
+      }
+      else{
+        throw new Error('Failed to login')
+      }
     })
     
   } catch (error) {
-    console.log(error);
+    document.body.innerHTML+= `<div style="red;">${error}</div>`
   }
  
-
-
 }
-
-
-
-
-async function DeleteUser(userId) {
-  try {
-    await axios.delete(`https://crudcrud.com/api/8d1572b7d4ce4589aa81bbf46ffb62cb/gun/${userId}`)
-    .then((response) => {
-      console.log(response)
-     
-      removeFromScreen(userId)
-      
-    })
-    
-    
-  } catch (error) {
-    console.log(error)
-  }
-
-
-}
-
-
-
- async function removeFromScreen(userId) {
-  try {
-    var parentNode =  document.getElementById('listOfUsers');
-    var childNodeToBeDeleted =  document.getElementById(userId);
-  
-    parentNode.removeChild(childNodeToBeDeleted);
-  
-    
-  } catch (error) {
-    console.log(error)
-  }
- 
-
-}
-
-
-
-
-
- function editUserDetails(Expenseamount, discription, categeory, userId) {
-  try {
-    document.getElementById('Expense').value = Expenseamount;
-  document.getElementById('discription').value = discription;
-  document.getElementById('list').value = categeory;
-  DeleteUser(userId)
-    
-  } catch (error) {
-    console.log(error)
-  }
-
-
-  
-
-}
-
-
-
- async function showUsersOnScreen(user) {
-  try {
-    var parentNode =await document.getElementById('listOfUsers');
-    var childNode = await `<li id=${user._id}>${user.Expenseamount}-${user.discription}-${user.categeory}
-           <button id="edit" onclick=editUserDetails('${user.Expenseamount}','${user.discription}','${user.categeory}','${user._id}')>Edit User</button>
-           <button id='${user._id}' onclick=DeleteUser('${user._id}')>Delete User</button></li>`
-    parentNode.innerHTML = parentNode.innerHTML + childNode;
-    
-  } catch (error) {
-    console.log(error)
-  }
-   
-  
- 
-  }
-
-
-
--- get.js
+++ get.js
@@ -1,63 +1,19 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
+const a = 10;
 
+{
 
-form.addEventListener('submit', addItem);
+const a = 20;
 
-itemList.addEventListener('click', removeItem);
+{
 
-filter.addEventListener('keyup', filterItems);
+const a = 50;
 
-function addItem(e){
-  e.preventDefault();
+console.log(a);
 
- 
-  var newItem = document.getElementById('item').value;
-
-
-  var li = document.createElement('li');
-  
-  li.className = 'list-group-item';
- 
-  li.appendChild(document.createTextNode(newItem));
-
-
-  var deleteBtn = document.createElement('button');
-
-  
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-  
-  deleteBtn.appendChild(document.createTextNode('X'));
-
- 
-  li.appendChild(deleteBtn);
-
- var editBtn = document.createElement('edit');
- editBtn.className = 'btn btn-grey btn-sm float-right edit';
- editBtn.appendChild(document.createTextNode('E'));
- li.appendChild(editBtn);
-
-  itemList.appendChild(li);
 }
 
+console.log(a)
 
-
-
-
-function filterItems(e){
- 
-  var text = e.target.value.toLowerCase();
- 
-  var items = itemList.getElementsByTagName('li');
-  
-  Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
 }
+
+console.log(a)

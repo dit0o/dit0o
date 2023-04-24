cae72815c4515f4c926bf21ecb3b7dbc76bf8d81
dit0o <gaganam6985@gmail.com>
2023-04-24 10:52:55 +0530

    premium

--- gagan/daily/daily .js
+++ gagan/daily/daily .js
@@ -82,3 +82,26 @@ async function showUsersOnScreen(expense) {
       console.log(error)
     }
 }
+document.getElementById("rzp-button1").onclick=async function(e){
+  const token=localStorage.getItem('token')
+  const response=await axios.get("http://localhost:3000/premium/member",{headers:{'Authorization':token}})
+var options={
+  "key":response.data.key_id,
+  "order_id":response.data.order.id,
+  "handler":async function(response){
+    await axios.post("http://localhost:3000/premium/updatestatus",{
+    order_id:options.order_id,
+    payment_id:response.razorpay_payment_id,
+    },{headers:{'Authorization':token}})
+    alert('you are a premium User now')
+  },
+};
+const rzp1=new Razorpay(options);
+rzp1.open();
+e.preventDefault();
+rzp1.on('payment.failed',function(response){
+  console.log(response)
+  alert('something went wrong')
+})
+
+}

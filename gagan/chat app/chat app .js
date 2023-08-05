a97a1ed157f7e621509d0b2856a54ae973e5d909
dit0o <gaganam6985@gmail.com>
2023-08-05 12:27:33 +0530

    chat 3

--- gagan/chat app/chat app .js
+++ gagan/chat app/chat app .js
@@ -1,36 +1,58 @@
+const inputObj=document.getElementById('input-send')
 const token=localStorage.getItem('token')
-async function formsubmit(event){
-    try {
-        event.preventDefault();
-        let inputObj={
-            message:document.getElementById('send-msg').value
-        }
-        await axios.post('http://localhost:4000/user/postmeg',inputObj, { headers: {'Authorization': token}} )
-        if(response===201){
+const sendMsg=document.getElementById('send-msg')
+
+inputObj.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let sendObj={message:sendMsg.value}
+    axios.post('http://localhost:4000/user/message',sendObj, { headers: {'Authorization': token}} )
+    .then((response)=>{
+        sendMsg.value='';
         addNewLineElement( response.data.mesg, response.data.Name );
-        }
-    }
-        
-    
-     catch (error) {
-       console.log(error) 
-    }
-}
-const msgUi=document.getElementById('messages-list');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+})
+
+
+const msgUl=document.getElementById('messages-list');
+
 function addNewLineElement(data,Name){
     const li=document.createElement('h4');
     li.appendChild(
         document.createTextNode(Name + ' : ' + data.message +' ')
     )
+    msgUl.appendChild(li)
+}
+let totalMsg=null;
+async function getAllMsg(){
+try {
+    const allM=await axios.get('http://localhost:4000/user/message', { headers: {'Authorization': token}} );
+    const arr = allM.data.mesg;
+    if(totalMsg!==arr.length){
+        msgUl.innerHTML="";
+        arr.forEach(element=>{
+            addNewLineElement(element,element.user.Name);
+        })
+    }
+    totalMsg=arr.length;
+   
+
+
+} catch (error) {
+    console.log(error)
+}
 }
 window.addEventListener('DOMContentLoaded', async()=>{
     try{
         const allM = await axios.get('http://localhost:4000/user/message', { headers: {'Authorization': token}} );
-        const Name = allM.data.Name;
         const arr = allM.data.mesg;
+        totalMsg=arr.length
         arr.forEach(element => {
-            addNewLineElement(element,Name);
+            addNewLineElement(element,element.user.Name);
         });
+        setInterval(getAllMsg,1000)
     }catch(err){
         console.log(err);
     }

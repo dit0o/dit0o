f16ec4d3da21c508ad21b870869b4d2ea2b0d1ef
dit0o <gaganam6985@gmail.com>
2023-08-08 21:25:24 +0530

    optimization

--- gagan/chat app/chat app .js
+++ gagan/chat app/chat app .js
@@ -1,14 +1,15 @@
 const inputObj=document.getElementById('input-send')
 const token=localStorage.getItem('token')
 const sendMsg=document.getElementById('send-msg')
+let totalMsg=null;
 
 inputObj.addEventListener('submit',(e)=>{
     e.preventDefault();
     let sendObj={message:sendMsg.value}
     axios.post('http://localhost:4000/user/message',sendObj, { headers: {'Authorization': token}} )
     .then((response)=>{
-        sendMsg.value='';
         addNewLineElement( response.data.mesg, response.data.Name );
+    
     })
     .catch((err) => {
         console.log(err);
@@ -25,18 +26,19 @@ function addNewLineElement(data,Name){
     )
     msgUl.appendChild(li)
 }
-let totalMsg=null;
+
 async function getAllMsg(){
 try {
-    const allM=await axios.get('http://localhost:4000/user/message', { headers: {'Authorization': token}} );
+    
+    const allM=await axios.get(`http://localhost:4000/user/message?lastId=${totalMsg}`, { headers: {'Authorization': token}} );
     const arr = allM.data.mesg;
-    if(totalMsg!==arr.length){
-        msgUl.innerHTML="";
+    if(arr.length>0){
+        totalMsg = totalMsg + arr.length;
         arr.forEach(element=>{
             addNewLineElement(element,element.user.Name);
         })
     }
-    totalMsg=arr.length;
+
    
 
 
@@ -46,7 +48,7 @@ try {
 }
 window.addEventListener('DOMContentLoaded', async()=>{
     try{
-        const allM = await axios.get('http://localhost:4000/user/message', { headers: {'Authorization': token}} );
+        const allM = await axios.get(`http://localhost:4000/user/message`, { headers: {'Authorization': token}} );
         const arr = allM.data.mesg;
         totalMsg=arr.length
         arr.forEach(element => {

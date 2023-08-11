d5c8cc76cda6d09c7020555542569b5957f47649
dit0o <gaganam6985@gmail.com>
2023-08-11 21:53:00 +0530

    making group

--- gagan/chat app/chat app .js
+++ gagan/chat app/chat app .js
@@ -2,11 +2,15 @@ const inputObj=document.getElementById('input-send')
 const token=localStorage.getItem('token')
 const sendMsg=document.getElementById('send-msg')
 let totalMsg=null;
+const box11=document.getElementById('box11')
+let activeGroup=null;
+let setIntId=null;
+
 
 inputObj.addEventListener('submit',(e)=>{
     e.preventDefault();
     let sendObj={message:sendMsg.value}
-    axios.post('http://localhost:4000/user/message',sendObj, { headers: {'Authorization': token}} )
+    axios.post(`http://localhost:4000/user/message?gId=${activeGroup}`,sendObj, { headers: {'Authorization': token}} )
     .then((response)=>{
         addNewLineElement( response.data.mesg, response.data.Name );
     
@@ -19,8 +23,14 @@ inputObj.addEventListener('submit',(e)=>{
 
 const msgUl=document.getElementById('messages-list');
 
-function addNewLineElement(data,Name){
+function addNewLineElement(data,Name,id){
     const li=document.createElement('h4');
+    loginId=localStorage.getItem('userId')
+    if(id==loginId){
+        li.className = 'msg-right';
+    } else {
+        li.className = 'msg-left';
+    }
     li.appendChild(
         document.createTextNode(Name + ' : ' + data.message +' ')
     )
@@ -30,12 +40,12 @@ function addNewLineElement(data,Name){
 async function getAllMsg(){
 try {
     
-    const allM=await axios.get(`http://localhost:4000/user/message?lastId=${totalMsg}`, { headers: {'Authorization': token}} );
+    const allM=await axios.get(`http://localhost:4000/user/message?lastId=${totalMsg}&gId=${activeGroup}`, { headers: {'Authorization': token}} );
     const arr = allM.data.mesg;
     if(arr.length>0){
         totalMsg = totalMsg + arr.length;
         arr.forEach(element=>{
-            addNewLineElement(element,element.user.Name);
+            addNewLineElement(element,element.user.Name,element.userId);
         })
     }
 
@@ -59,3 +69,25 @@ window.addEventListener('DOMContentLoaded', async()=>{
         console.log(err);
     }
 });
+
+const groupBtn=document.getElementById('create-group-btn')
+const invitBtn=document.getElementById('invite-btn')
+const groupFormDiv=document.getElementById("create-Group-Div")
+const groupForm=document.getElementById("create-Group-Form")
+
+groupBtn.onclick=function(){
+groupFormDiv.removeAttribute('hidden')
+}
+groupForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const gName = document.getElementById('new-Group-Name');
+    let gNameObj={gName:gName.value}
+    axios.post('http://localhost:4000/user/message/creategroup', gNameObj, { headers: {'Authorization': token}})
+    .then((response) => {
+        console.log(response);
+        window.location.reload();
+    }).catch((err) => {
+        console.log(err);
+    });
+});
+

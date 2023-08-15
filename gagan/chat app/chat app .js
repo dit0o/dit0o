0dc3842cc59e1b1c576bd0b49d04f08da311d8d0
dit0o <gaganam6985@gmail.com>
2023-08-15 22:22:18 +0530

    get user admin

--- gagan/chat app/chat app .js
+++ gagan/chat app/chat app .js
@@ -6,6 +6,9 @@ const box11=document.getElementById('box11')
 let activeGroup=null;
 let setIntId=null;
 const inviteBtn=document.getElementById('invite-btn')
+const addUserBtn = document.getElementById('add-user-btn');
+const usersList = document.getElementById('users-list');
+const usersBox = document.getElementById('users-box');
 
 
 inputObj.addEventListener('submit',(e)=>{
@@ -63,28 +66,35 @@ window.addEventListener('DOMContentLoaded', async()=>{
             li.type = 'button';
             li.value = `${ele.group.gName}`;
             li.addEventListener('click',async()=>{
-                activeGroup = ele.id;
+                inviteBtn.setAttribute('hidden');
+                addUserBtn.setAttribute('hidden');
+                activeGroup = ele.groupId;
                 box11.removeAttribute('hidden');
+                usersBox.removeAttribute('hidden');
+                if(ele.isAdmin){
+                    inviteBtn.removeAttribute('hidden');
+                    addUserBtn.removeAttribute('hidden');
+                }
+                
                 inviteBtn.removeAttribute('hidden');
                 msgUl.innerHTML='';
                clearInterval(setIntId);
-                const allM = await axios.get(`http://localhost:4000/user/message?gId=${ele.id}`, { headers: {'Authorization': token}} );
+                const allM = await axios.get(`http://localhost:4000/user/message?gId=${ activeGroup}`, { headers: {'Authorization': token}} );
                 const arr = allM.data.mesg;
                 totalMsg = arr.length;
                 arr.forEach(element => {
                     addNewLineElement(element, element.user.Name, element.userId);
                 });
+                const allU = await axios.get(`http://localhost:4000/admin/allUsers?gId=${activeGroup}`, { headers: {'Authorization': token}});
+                const arr2 = allU.data.allUsers;
+                arr2.forEach(ele=>{
+                     addNewUserElement(ele,ele.isAdmin,ele.userId);
+                })
                 setIntId = setInterval(getAllMsg, 2000);
             })
             allGDiv.appendChild(li);
         })
-      //  const allM = await axios.get(`http://localhost:4000/user/message`, { headers: {'Authorization': token}} );
-       // const arr = allM.data.mesg;
-        //totalMsg=arr.length
-        //arr.forEach(element => {
-           // addNewLineElement(element,element.user.Name);
-       // });
-       // setInterval(getAllMsg,1000)
+    
     }catch(err){
         console.log(err);
     }
@@ -138,6 +148,18 @@ joinGroupFrom.addEventListener('submit',async(e)=>{
     const decodeToken = parseJwt(tokenInput.value);
     const id = +decodeToken.id;
     console.log('joinGroupForm',id);
-    const joinRes = await axios.get(`http:localhost:4000/user/message/joinGroup?gId=${id}`, { headers: {'Authorization': token}});
+    const joinRes = await axios.get(`http://localhost:4000/user/message/joinGroup?gId=${id}`, { headers: {'Authorization': token}});
     console.log(joinRes);
+});
+
+addUserBtn.addEventListener('click', ()=>{
+    document.getElementById('add-user-div').removeAttribute('hidden');
+});
+const addUserForm = document.getElementById('add-user-form');
+addUserForm.addEventListener('submit',async (e)=>{
+    e.preventDefault();
+    const addUserBy = document.getElementById('add-user-by').value;
+    const addUserValue = document.getElementById('add-user-value').value;
+    const addUserRes = await axios.get(`http://localhost:4000/admin/addUser?by=${addUserBy}&value=${addUserValue}&gId=${activeGroup}`, { headers: {'Authorization': token}});
+    console.log(addUserRes.data.groupmem);
 });
